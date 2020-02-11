d83369761b3c25783bfe02e05ec30dfc2687cd68
Patrick Smith <patrick@burntcaramel.com>
2020-02-11 18:13:54 +1100

    Add vary() and fresh()

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,14 +1,13 @@
-import { lazy } from './index';
+import { lazy, vary, fresh } from './index';
 
 describe('lazy', () => {
   const creator = jest.fn().mockReturnValue({
     three: 3,
   });
-  const subject = lazy(creator);
-
   beforeEach(() => {
     creator.mockClear();
   });
+  const subject = lazy(creator);
 
   describe('when called as function', () => {
     it('returns value of creator', () => {
@@ -34,7 +33,7 @@ describe('lazy', () => {
     });
   });
 
-  describe('when it relies on another lazy value', () => {
+  describe('when one lazy relies on another lazy value', () => {
     const inner = lazy(() => ({
       isInner: true,
     }));
@@ -46,4 +45,69 @@ describe('lazy', () => {
       expect(outer()).toEqual({ inner: { isInner: true } });
     });
   });
+});
+
+describe('vary()', () => {
+  describe('override value in test', () => {
+    const subject = vary(5);
+
+    it('is 5', () => {
+      expect(subject()).toEqual(5);
+    });
+
+    describe('override with 9', () => {
+      new subject(9);
+
+      it('is 9', () => {
+        expect(subject()).toEqual(9);
+      });
+
+      describe('override with 12', () => {
+        new subject(12);
+
+        it('is 12', () => {
+          expect(subject()).toEqual(12);
+        });
+      });
+
+      it('is still 9', () => {
+        expect(subject()).toEqual(9);
+      });
+    });
+
+    it('is still 5', () => {
+      expect(subject()).toEqual(5);
+    });
+  });
+});
+
+describe('fresh()', () => {
+  const objects = fresh(jest.fn, mock => mock.mockClear());
+
+  it('has infinite length', () => {
+    expect(objects.length).toEqual(Infinity);
+  });
+
+  const [a, b, c] = objects;
+  it.each([a, b, c])('object is mock', object => {
+    expect(jest.isMockFunction(object)).toBe(true);
+  });
+
+  describe('when calling a', () => {
+    beforeEach(() => {
+      a('argument');
+    });
+
+    it('has not called b', () => {
+      expect(b).toHaveBeenCalledTimes(0);
+    });
+  });
+
+  describe("when calling result", () => {
+    const result = objects();
+
+    it("is mock", () => {
+      expect(jest.isMockFunction(result)).toBe(true);
+    })
+  })
 });
--- src/index.ts
+++ src/index.ts
@@ -5,27 +5,83 @@ export function lazy<T>(creator: () => T): T & (() => T) {
     current = undefined;
   });
 
-  return new Proxy(
-    () => {},
-    {
-      get(_obj: {}, prop: keyof T) {
-        if (prop === 'calls') {
-          return;
-        }
-
-        if (!current) {
-          current = creator();
-        }
-
-        return current[prop];
-      },
-      apply() {
-        if (!current) {
-          current = creator();
-        }
-  
-        return current;
+  return new Proxy(() => {}, {
+    get(_obj: {}, prop: keyof T) {
+      if (prop === 'calls') {
+        return;
       }
+
+      if (!current) {
+        current = creator();
+      }
+
+      return current[prop];
     },
-  ) as T & (() => T);
+    apply() {
+      if (!current) {
+        current = creator();
+      }
+
+      return current;
+    },
+  }) as T & (() => T);
+}
+
+export function vary<T>(
+  initialValue: T
+): (() => T) & {
+  new (newValue: T): void;
+} {
+  let currentValue = initialValue;
+
+  beforeEach(() => {
+    currentValue = initialValue;
+  });
+
+  return new Proxy(() => {}, {
+    apply() {
+      return currentValue;
+    },
+    construct(_target: {}, [newValue]) {
+      beforeEach(() => {
+        currentValue = newValue;
+      });
+      return {};
+    },
+  }) as (() => T) & {
+    new (newValue: T): void;
+  };
+}
+
+export function fresh<T>(
+  creator: () => T,
+  refresher: (object: T) => void
+): Array<T> & (() => T) {
+  return new Proxy(() => {}, {
+    get(_obj: {}, prop) {
+      if (prop === 'length') {
+        return Infinity;
+      }
+
+      if (typeof prop === 'string') {
+        const index = parseInt(prop, 10);
+        if (index.toString() === prop) {
+          const object = creator();
+          beforeEach(() => {
+            refresher(object);
+          });
+          return object;
+        }
+      }
+
+      return undefined;
+    },
+    apply() {
+      const object = creator();
+      beforeEach(() => {
+        refresher(object);
+      });
+      return object;
+    },
+  }) as Array<T> & (() => T);
 }
